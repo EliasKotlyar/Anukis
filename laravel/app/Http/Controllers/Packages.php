326993a47f196a182b3155533123f4e391f1232b
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-02-19 12:05:14 +0100

    Packages

--- laravel/app/Http/Controllers/Packages.php
+++ laravel/app/Http/Controllers/Packages.php
@@ -6,10 +6,36 @@ use Illuminate\Http\Request;
 
 class Packages extends Controller
 {
-    public function listJson(){
+    public function downloadPackage(){
+
+    }
+    public function getLinks()
+    {
+
+
+
+    }
+
+    public function listJson()
+    {
+        $packages = [];
+        foreach ($this->getLinks() as $composerName => $link) {
+            $packages[$composerName] = array(
+                'dev-master' => array(
+                    'version' => 'dev-master',
+                    'name' => $composerName,
+                    'dist' => array(
+
+                        'type' => 'zip',
+                        'url' => $link
+                    )
+                )
+            );
+        }
+
+
         return response()->json([
-            'name' => 'Abigail',
-            'state' => 'CA'
+            'packages' => $packages,
         ]);
     }
     //
